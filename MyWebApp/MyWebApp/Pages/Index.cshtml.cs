3f1ba5f2f4123797d6a73a2623695d0676abe965
LTAYLOR10 <LTAYLOR10@LTWT630029>
2021-11-12 15:07:10 +0000

    added two new pages

--- MyWebApp/MyWebApp/Pages/Index.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyWebApp.Pages
 {
-    public class Home : PageModel
+    public class Index : PageModel
     {
+        public string testField { get; set; }
+
         public void OnGet()
         {
-            
+            testField = System.DateTime.Now.ToString();
+        }
+
+        public IActionResult OnPost(string getName)
+        {
+            return RedirectToPage("./Hello", new { name = getName });
         }
     }
 }
