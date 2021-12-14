7526d5e664327a0754c829df59e78e5c485d480d
LTAYLOR10 <LTAYLOR10@LTWT630029>
2021-12-14 13:58:15 +0000

    page updates

--- MyWebApp/MyWebApp/Pages/Project/Index.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Project/Index.cshtml.cs
@@ -7,99 +7,18 @@ namespace MyWebApp.Pages.Project
 {
     public class Index : PageModel
     {
+        public string OutgoingAccademyUrn { get; set; }
+        public string OutgoingAccademyName { get; set; }
+
         public void OnGet()
         {
             
         }
     }
-    public class ProjectViewModel
-    {
-        public TestProjectData Project { get; set; }
+  
+        
 
-    }
+    
 }
 
 
-public class ProjectTaskListViewModel : ProjectViewModel
-{
-    public EducationPerformance EducationPerformance { get; set; }
-    public ProjectStatuses FeatureTransferStatus => GetFeatureTransferStatus();
-    public ProjectStatuses TransferDatesStatus => GetTransferDatesStatus();
-    public ProjectStatuses BenefitsAndOtherFactorsStatus => GetBenefitsAndOtherFactorsStatus();
-    public ProjectStatuses RationaleStatus => GetRationaleStatus();
-    public ProjectStatuses AcademyAndTrustInformationStatus() => GetAcademyAndTrustInformationStatus();
-    public bool HasKeyStage2PerformanceInformation => PerformanceDataHelpers.HasKeyStage2PerformanceInformation(EducationPerformance.KeyStage2Performance);
-    public bool HasKeyStage4PerformanceInformation =>
-        PerformanceDataHelpers.HasKeyStage4PerformanceInformation(EducationPerformance.KeyStage4Performance);
-
-    public bool HasKeyStage5PerformanceInformation =>
-        PerformanceDataHelpers.HasKeyStage5PerformanceInformation(EducationPerformance.KeyStage5Performance);
-
-    private ProjectStatuses GetAcademyAndTrustInformationStatus()
-    {
-        var academyAndTrustInformation = Project.AcademyAndTrustInformation;
-        return (string.IsNullOrEmpty(academyAndTrustInformation.Author) &&
-                (academyAndTrustInformation.Recommendation ==
-                 TransferAcademyAndTrustInformation.RecommendationResult.Empty)) ? ProjectStatuses.NotStarted
-            : (!string.IsNullOrEmpty(academyAndTrustInformation.Author) &&
-               (academyAndTrustInformation.Recommendation !=
-                TransferAcademyAndTrustInformation.RecommendationResult.Empty)) ? ProjectStatuses.Completed
-            : ProjectStatuses.InProgress;
-    }
-
-    private ProjectStatuses GetFeatureTransferStatus()
-    {
-        if (Project.Features.WhoInitiatedTheTransfer == TransferFeatures.ProjectInitiators.Empty &&
-            Project.Features.ReasonForTransfer.IsSubjectToRddOrEsfaIntervention == null &&
-            Project.Features.TypeOfTransfer == TransferFeatures.TransferTypes.Empty)
-            return ProjectStatuses.NotStarted;
-
-        if (Project.Features.WhoInitiatedTheTransfer != TransferFeatures.ProjectInitiators.Empty &&
-            Project.Features.ReasonForTransfer.IsSubjectToRddOrEsfaIntervention != null &&
-            Project.Features.TypeOfTransfer != TransferFeatures.TransferTypes.Empty)
-            return ProjectStatuses.Completed;
-
-        return ProjectStatuses.InProgress;
-    }
-
-    private ProjectStatuses GetTransferDatesStatus()
-    {
-        if ((string.IsNullOrEmpty(Project.Dates.FirstDiscussed) && (Project.Dates.HasFirstDiscussedDate ?? true)) &&
-            (string.IsNullOrEmpty(Project.Dates.Target) && (Project.Dates.HasTargetDateForTransfer ?? true)) &&
-            (string.IsNullOrEmpty(Project.Dates.Htb) && (Project.Dates.HasHtbDate ?? true)))
-            return ProjectStatuses.NotStarted;
-
-        if ((!string.IsNullOrEmpty(Project.Dates.FirstDiscussed) || Project.Dates.HasFirstDiscussedDate == false) &&
-            (!string.IsNullOrEmpty(Project.Dates.Target) || Project.Dates.HasTargetDateForTransfer == false) &&
-            (!string.IsNullOrEmpty(Project.Dates.Htb) || Project.Dates.HasHtbDate == false))
-            return ProjectStatuses.Completed;
-
-        return ProjectStatuses.InProgress;
-    }
-
-    private ProjectStatuses GetBenefitsAndOtherFactorsStatus()
-    {
-        if ((Project.Benefits.IntendedBenefits == null || !Project.Benefits.IntendedBenefits.Any()) &&
-            (Project.Benefits.OtherFactors == null || !Project.Benefits.OtherFactors.Any()))
-            return ProjectStatuses.NotStarted;
-
-        if ((Project.Benefits.IntendedBenefits != null && Project.Benefits.IntendedBenefits.Any()) &&
-            (Project.Benefits.OtherFactors != null && Project.Benefits.OtherFactors.Any()))
-            return ProjectStatuses.Completed;
-
-        return ProjectStatuses.InProgress;
-    }
-
-    private ProjectStatuses GetRationaleStatus()
-    {
-        if (string.IsNullOrEmpty(Project.Rationale.Project) &&
-            string.IsNullOrEmpty(Project.Rationale.Trust))
-            return ProjectStatuses.NotStarted;
-
-        if (!string.IsNullOrEmpty(Project.Rationale.Project) &&
-            !string.IsNullOrEmpty(Project.Rationale.Trust))
-            return ProjectStatuses.Completed;
-
-        return ProjectStatuses.InProgress;
-    }
-}
