48806e69c105c3fb2e8a7f416a83609b49f4681b
LTAYLOR10 <LTAYLOR10@LTWT630029>
2021-12-14 15:37:14 +0000

    add task list page

--- MyWebApp/MyWebApp/Pages/Project/Index.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Project/Index.cshtml.cs
@@ -9,6 +9,17 @@ namespace MyWebApp.Pages.Project
     {
         public string OutgoingAccademyUrn { get; set; }
         public string OutgoingAccademyName { get; set; }
+        public string Project { get; set; }
+        public string Urn { get; set; }
+        public bool HasKeyStage2PerformanceInformation { get; set; }
+        public bool HasKeyStage4PerformanceInformation { get; set; }
+        public bool HasKeyStage5PerformanceInformation { get; set; }
+
+        public ProjectStatuses TransferDatesStatus { get; set; }
+        public ProjectStatuses BenefitsAndOtherFactorsStatus { get; set; }
+        public ProjectStatuses RationaleStatus { get; set; }
+        public ProjectStatuses AcademyAndTrustInformationStatus { get; set; }
+
         public ProjectStatuses FeatureTransferStatus { get; set; }
 
         public void OnGet()
