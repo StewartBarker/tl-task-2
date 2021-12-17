24a32bb693de09ef4551b4f2100167387ab6436a
StewartBarker <stew.barker@outlook.com>
2021-12-17 09:58:31 +0000

    add index page

--- MyWebApp/MyWebApp/Pages/Project/Index.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Project/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Data;
 using Data.Models;
 using Data.Models.KeyStagePerformance;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,14 +8,19 @@ namespace MyWebApp.Pages.Project
 {
     public class Index : PageModel
     {
+        private readonly IProjects _projectRepository;
+
+        public Index(IProjects projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+        
         public string OutgoingAccademyUrn { get; set; }
         public string OutgoingAccademyName { get; set; }
-        public string Project { get; set; }
         public string Urn { get; set; }
         public bool HasKeyStage2PerformanceInformation { get; set; }
         public bool HasKeyStage4PerformanceInformation { get; set; }
         public bool HasKeyStage5PerformanceInformation { get; set; }
-
         public ProjectStatuses TransferDatesStatus { get; set; }
         public ProjectStatuses BenefitsAndOtherFactorsStatus { get; set; }
         public ProjectStatuses RationaleStatus { get; set; }
@@ -24,7 +30,14 @@ namespace MyWebApp.Pages.Project
 
         public void OnGet()
         {
-            
+            var result = _projectRepository.GetByUrn(Urn).Result;
+
+            if (result.IsValid)
+            {
+                OutgoingAccademyUrn = result?.Result?.OutgoingAcademyUrn;
+                OutgoingAccademyName = result?.Result?.OutgoingAcademyName;
+            }
+
         }
     }
   
