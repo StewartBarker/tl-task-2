8a89584a9a7c4e6806637988650c6fcea2c6734b
LTAYLOR10 <LTAYLOR10@LTWT630029>
2022-01-11 14:49:57 +0000

    add new pages inside transfer dates

---
+++ MyWebApp/MyWebApp/Pages/Projects/TransferDates/FirstDiscussed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages.Projects.TransferDates
+{
+    public class FirstDiscussedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyWebApp/MyWebApp/Pages/Projects/TransferDates/TargetDateForTransfer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages.Projects.TransferDates
+{
+    public class TargetDateForTransferModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
