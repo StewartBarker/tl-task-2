89872d1e6fe98f3af3cfcdf2151aa120748c7d9b
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-01-14 15:09:34 +0000

    Add post method

--- MyWebApp/MyWebApp/Pages/Projects/TransferDates/FirstDiscussed.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Projects/TransferDates/FirstDiscussed.cshtml.cs
@@ -11,10 +11,6 @@ namespace MyWebApp.Pages.Projects.TransferDates
         [BindProperty(SupportsGet = true)]
         public string Urn { get; set; }
         public DateTime FirstDiscussed { get; set; }
-
-        public string Date { get; set; }
-
-        public string ModelValidationState { get; set; }
         public string ReturnToPreview { get; set; }
         public string OutgoingAcademyName { get; set; }
 
@@ -25,6 +21,11 @@ namespace MyWebApp.Pages.Projects.TransferDates
             var projectRepository = new MockProjectRepository();
             var project = projectRepository.GetByUrn(Urn);
             FirstDiscussed = DateTime.Parse(project.Result.Result.Dates.FirstDiscussed);
+        }
+
+        public void onPost()
+        {
+            
         }
     }
 }
