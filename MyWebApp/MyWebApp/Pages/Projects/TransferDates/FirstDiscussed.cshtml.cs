b1ae2b76f15002642018508e783e3a8eaf5212e6
LTAYLOR10 <LTAYLOR10@LTWT630029>
2022-01-14 14:46:13 +0000

    add content to pages in transfer dates

--- MyWebApp/MyWebApp/Pages/Projects/TransferDates/FirstDiscussed.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Projects/TransferDates/FirstDiscussed.cshtml.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace MyWebApp.Pages.Projects.TransferDates
 {
     public class FirstDiscussedModel : PageModel
     {
+
+
+        public string Urn { get; set; }
+        public DateTime FirstDiscussed { get; set; }
+
+        public string Date { get; set; }
+
+        public string ModelValidationState { get; set; }
+        public string ReturnToPreview { get; set; }
+        public string OutgoingAcademyName { get; set; }
+
+        public bool UnknownDate { get; set; }
+
+
         public void OnGet()
         {
         }
--- MyWebApp/MyWebApp/Pages/Projects/TransferDates/SetAdvisoryBoardDate.cshtml.cs
+++ MyWebApp/MyWebApp/Pages/Projects/TransferDates/SetAdvisoryBoardDate.cshtml.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace MyWebApp.Pages.Projects.TransferDates
 {
     public class SetAdvisoryBoardDateModel : PageModel
     {
+
+        public string Urn { get; set; }
+        public DateTime HtbDate { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string ModelValidationState { get; set; }
+        public string ReturnToPreview { get; set; }
+        public string OutgoingAcademyName { get; set; }
+
+        public bool UnknownDate { get; set; }
         public void OnGet()
         {
         }
